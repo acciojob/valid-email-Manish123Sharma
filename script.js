733ed5f2aebca2fc249bcea2b1f75ee02e9d2be7
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-13 13:38:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// function validEmail(str) {
+//   //your JS code here.
+// }
+
+// // Do not change the code below.
+// // const str = prompt("Enter an email address.");
+// alert(validEmail(str));
+
+
 function validEmail(str) {
-  //your JS code here.
+  if (!str) return false;
+
+  // Regex explanation:
+  // ^                     : start of string
+  // [\w.-]+               : one or more word characters, dots or hyphens
+  // @                     : at symbol
+  // [\w-]+(\.[\w-]+)*     : domain name with optional subdomains
+  // \.[a-zA-Z]{2,}        : a period followed by 2 or more letters (domain extension)
+  // $                     : end of string
+  const regex = /^[\w.-]+@[\w-]+(\.[\w-]+)*\.[a-zA-Z]{2,}$/;
+
+  return regex.test(str);
 }
 
 // Do not change the code below.
